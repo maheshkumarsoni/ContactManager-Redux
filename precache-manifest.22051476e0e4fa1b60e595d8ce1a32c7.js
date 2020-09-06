de419b5225ffc2a43188f92e90008b03dc12dc22
maheshkumarsoni <sms3299sms@gmail.com>
2020-09-06 15:52:37 +0530

    Updates

---
+++ precache-manifest.22051476e0e4fa1b60e595d8ce1a32c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42676a6d133728aabd3674e63ea6575c",
+    "url": "/ContactManager-Redux/index.html"
+  },
+  {
+    "revision": "d4c4d74577a64f222fd4",
+    "url": "/ContactManager-Redux/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d4c4d74577a64f222fd4",
+    "url": "/ContactManager-Redux/static/js/2.affd88fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ContactManager-Redux/static/js/2.affd88fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbf7bfd1a0530a3e51c",
+    "url": "/ContactManager-Redux/static/js/main.d5eaa1f8.chunk.js"
+  },
+  {
+    "revision": "1eb21a01d60cd5a7535e",
+    "url": "/ContactManager-Redux/static/js/runtime-main.22fc27a1.js"
+  }
+]);
